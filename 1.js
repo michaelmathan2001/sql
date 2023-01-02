9413eea8c8558a0fcab30b922609feb26c35d892
michaelmathan2001 <michaelmathan2001@gmail.com>
2023-01-02 10:01:09 +0530

    new commit

---
+++ 1.js
@@ -0,0 +1,30 @@
+var name="ABS"
+console.log(name)
+
+let a=6
+let b=6
+c=a+b
+d=a-b
+e=a/b
+f=a%b
+console.log(c)
+console.log(d)
+console.log(e)
+console.log(f)
+
+const num=2
+// constant value cannot be changed
+console.log(num)
+
+var x=6
+x++
+console.log(x)
+var y=9
+y--
+console.log(y)
+var z=5
+++z
+console.log(z)
+var w=4
+--w
+console.log(w)
